97aa11833b0c1cd33d6c8c7a389c58111ef38d90
RostilKant <rostilnytskiy@gmail.com>
2020-09-10 01:39:03 +0300

    Adding home & pokemon pages

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PokemonPageComponent} from './pokemon-page/pokemon-page.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      // {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent, pathMatch: 'full'},
+      {path: 'pokemon/:id', component: PokemonPageComponent}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import {SharedModule} from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
+    HomePageComponent,
+    PokemonPageComponent,
   ],
   imports: [
     BrowserModule,
