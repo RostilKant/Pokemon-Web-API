e9db4699c16ebbca9c79dc1bed84b866fdc562e7
RostilKant <rostilnytskiy@gmail.com>
2020-09-10 19:21:04 +0300

    UserName is used for login

--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -16,22 +16,21 @@ namespace Entities.Models
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         
+        [Required(ErrorMessage = "Username is required")]
         public string? UserName { get; set; }
-        
-        [Required(ErrorMessage = "Email is required")]
-        public string? Email { get; set; }
         
         [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
         
+        public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public ICollection<string>? Roles { get; set; }
     }
     
     public class UserForAuthenticationDto
     {
-        [Required(ErrorMessage = "Email is required")]
-        public string? Email { get; set; }
+        [Required(ErrorMessage = "User name is required")]
+        public string? UserName { get; set; }
         
         [Required(ErrorMessage = "Password name is required")]
         public string? Password { get; set; }
