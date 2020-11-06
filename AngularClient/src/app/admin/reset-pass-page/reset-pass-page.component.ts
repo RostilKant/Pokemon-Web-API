d24c1d698fa71191c31c6dc2d2118de6b395cbff
RostilKant <rostilnytskiy@gmail.com>
2020-11-06 23:44:11 +0200

    Adding screenshots

---
+++ AngularClient/src/app/admin/reset-pass-page/reset-pass-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-pass-page',
+  templateUrl: './reset-pass-page.component.html',
+  styleUrls: ['./reset-pass-page.component.scss']
+})
+export class ResetPassPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
